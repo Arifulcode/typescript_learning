9bd542f71b2f744807237456405585f31fc9e797
Ariful Islam Polash <arifulislampolash.25.fswad@gmail.com>
2022-11-22 11:48:23 +0600

    npm init and ts save mode install with folder modified

---
+++ firstProgram/index.ts
@@ -0,0 +1,23 @@
+function addition(num1,num2){
+    console.log(num1 + num2);
+}
+function subtraction(num1,num2){
+    console.log(num1 - num2);
+}
+
+
+function multiplication(num1,num2){
+    console.log(num1 * num2);
+}
+
+function division(num1,num2){
+    console.log(num1 / num2);
+}
+addition(30,20);
+subtraction(30, 20);
+multiplication(30,20);
+division(30,20);
+
+let userName:"Ariful";
+// userName= string;
+console.log('userName'); 
