9c86ca6978f47111a9a0772b2b47e4812c17af77
Ariful Islam Polash <arifulislampolash.25.fswad@gmail.com>
2022-11-22 13:21:55 +0600

    Array Userdefined type practise done

---
+++ userdefined_types_ts/arrayType.js
@@ -0,0 +1,16 @@
+var studentNames;
+var studnentNames;
+var studentId;
+var MultipleTypes;
+studentNames = ["md", "ariful", "islam", "polash"];
+studentNames.push("zamil", "hobby", "nabil", "zomir");
+studentNames.pop();
+studentNames.sort();
+MultipleTypes = ["name", 1, 0, 20, 37, "jahirul", "amena", true];
+console.log(studentNames);
+console.log(studentNames[2]);
+console.log(MultipleTypes[1]);
+console.log(MultipleTypes[0]);
+console.log(MultipleTypes);
+MultipleTypes.sort();
+console.log(MultipleTypes);
---
+++ userdefined_types_ts/arrayType.ts
@@ -0,0 +1,28 @@
+let studentNames:string[];
+let studnentNames: Array<string>;
+let studentId:number;
+
+let MultipleTypes:(string | number | boolean) [];
+
+studentNames = ["md","ariful","islam","polash"];
+
+studentNames.push("zamil","hobby","nabil","zomir");
+studentNames.pop();
+studentNames.sort();
+
+
+
+MultipleTypes=["name",1,0,20,37,"jahirul","amena",true];
+
+console.log(studentNames);
+console.log(studentNames[2]);
+
+console.log(MultipleTypes[1]);
+console.log(MultipleTypes[0]);
+console.log(MultipleTypes);
+MultipleTypes.sort();
+console.log(MultipleTypes);
+
+
+
+
